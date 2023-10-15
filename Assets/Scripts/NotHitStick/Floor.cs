343ede3bc9736190daeae1bc3564758769a7b1bb
KomuRui <103985106+KomuRui@users.noreply.github.com>
2023-10-15 15:03:27 +0900

    プレイヤーと触れている間時間が減少するように

--- Assets/Scripts/NotHitStick/Floor.cs
+++ Assets/Scripts/NotHitStick/Floor.cs
@@ -6,11 +6,16 @@ using TMPro;
 public class Floor : MonoBehaviour
 {
 
-    //制限時間テキスト
-    [SerializeField] private TextMeshProUGUI[] timeTextMeshPro;
+    [SerializeField] private TextMeshProUGUI[] timeTextMeshPro; //制限時間テキスト
 
     //現在制限時間
     private float time = 30.0f;
+
+    //スピード倍率
+    private float speedRatio = 1.0f;
+
+    //当たっているプレイヤーを保管
+    private List<GameObject> hitPlayer = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +28,42 @@ public class Floor : MonoBehaviour
     void Update()
     {
         //時間計算・表示
-        time -= Time.deltaTime;
+        time -= Time.deltaTime * (speedRatio * hitPlayer.Count);
         for (int i = 0; i < timeTextMeshPro.Length; i++)
             timeTextMeshPro[i].text = ((int)time).ToString();
+
+    }
+
+    // コリジョンが発生したときに呼び出されるメソッド
+    private void OnCollisionEnter(Collision collision)
+    {
+        //プレイヤーではないのならこの先処理しない
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        //すでに同じプレイヤーがいないかチェック
+        for (int i = 0; i < hitPlayer.Count; i++)
+            if (hitPlayer[i] == collision.gameObject) return;
+
+        //追加
+        hitPlayer.Add(collision.gameObject);
+    }
+
+    //コリジョンが離れた時に呼ばれる関数
+    private void OnCollisionExit(Collision collision)
+    {
+        //プレイヤーではないのならこの先処理しない
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        //同じプレイヤーがいないかチェック
+        for (int i = 0; i < hitPlayer.Count; i++)
+        {
+            if (hitPlayer[i] == collision.gameObject)
+            {
+                //削除
+                hitPlayer.Remove(collision.gameObject);
+                return;
+            }
+        }
+       
     }
 }
