80dd3b34bc6a1b4d5a6c4d8eeda0733a99a69148
琉偉 小村 <210465@jc-21.jp>
2023-10-19 10:33:26 +0900

    paint途中

--- Assets/Scripts/Let'sPaint/LetsPaintPlayer.cs
+++ Assets/Scripts/Let'sPaint/LetsPaintPlayer.cs
@@ -4,15 +4,89 @@ using UnityEngine;
 
 public class LetsPaintPlayer : MonoBehaviour
 {
+
+    //アニメーションに必要
+    public enum SlimeAnimationState { Idle, Walk, Jump, Attack, Damage }
+
+    public Face faces;
+    public GameObject SmileBody;
+    public SlimeAnimationState currentState;
+
+    public Animator animator;
+    public int damType;
+
+    private Material faceMaterial;
+
+    [SerializeField] private float moveSpeed = 5.0f;          // プレイヤーの移動速度
+    [SerializeField] private float rotationSpeed = 180.0f;    // プレイヤーの回転速度
+    [SerializeField] private float gravitySpeed = 0.05f;      // 重力速度
+    [SerializeField] private bool isHorizontalInput = true;   // 横の入力許可するか
+    [SerializeField] private bool isVerticalInput = true;     // 縦の入力許可するか
+    [SerializeField] private bool isAnimIdle = true;
+    [SerializeField] private bool isAnimWalk = true;
+    [SerializeField] private bool isAnimJump = true;
+    [SerializeField] private bool isAnimAttack = true;
+    [SerializeField] private bool isAnimDamage = true;
+    [SerializeField] private int playerNum;                   // プレイヤー番号
+    [SerializeField] private Vector3 localGravity;
+    private Rigidbody rBody;
+    private Transform mainCameraTransform; // メインカメラのTransform
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //マテリアル設定
+        //faceMaterial = SmileBody.GetComponent<Renderer>().materials[1];
+
+        // メインカメラを取得
+        mainCameraTransform = Camera.main.transform;
+
+        //リジットボディ取得
+        rBody = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    //移動
+    private void Move()
+    {
+
+        // 入力を取得用
+        float horizontalInput = 0;
+        float verticalInput = 0;
+
+        // 入力を取得
+        if (isHorizontalInput) horizontalInput = Input.GetAxis("L_Stick_H" + playerNum);
+        if (isVerticalInput) verticalInput = -Input.GetAxis("L_Stick_V" + playerNum);
+
+        //入力がないのなら
+        if (horizontalInput == 0 && verticalInput == 0)
+        {
+            //通常状態に変更
+           // ChangeStateTo(SlimeAnimationState.Idle);
+            return;
+        }
+
+        //歩き状態に変更
+       // ChangeStateTo(SlimeAnimationState.Walk);
+
+        // カメラの向きを基準にプレイヤーを移動
+        Vector3 forwardDirection = mainCameraTransform.forward;
+        Vector3 rightDirection = mainCameraTransform.right;
+        forwardDirection.y = 0f; // Y軸成分を0にすることで水平方向に制限
+
+        // 移動方向を計算
+        Vector3 moveDirection = (forwardDirection.normalized * verticalInput + rightDirection.normalized * horizontalInput).normalized;
+
+        // 移動
+        rBody.AddForce(moveDirection * moveSpeed * Time.deltaTime);
+        //transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
     }
 }
