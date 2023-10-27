e88d31fa8dadac7c6ca9b2285afa209fbc3d741c
三浦暁 <210525@jc-21.jp>
2023-10-27 17:03:02 +0900

    hozonn

--- Assets/Scripts/SurfBoard/SurfboardPlayer.cs
+++ Assets/Scripts/SurfBoard/SurfboardPlayer.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEditor.PlayerSettings;
 
 public class SurfboardPlayer : MonoBehaviour
 {
     [SerializeField] private int playerNum;                   // プレイヤー番号
+    [SerializeField] private float LIMIT_ROTATE = 90.0f;      //回転の限界
     private Rigidbody rb;
     private Vector3 pos;
+    Quaternion rot;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        rot = this.transform.rotation;
     }
 
     // Update is called once per frame
@@ -26,6 +31,13 @@ public class SurfboardPlayer : MonoBehaviour
         horizontalInput = Input.GetAxis("L_Stick_H" + playerNum);
         verticalInput = -Input.GetAxis("L_Stick_V" + playerNum);
 
+        //スティックによって回転
         transform.Rotate(new Vector3(0, horizontalInput, 0));
+        transform.Rotate(new Vector3(verticalInput , 0, 0));
+
+        if (transform.localEulerAngles.x >= 0)        
+        {
+            int a = 0;
+        }
     }
 }
