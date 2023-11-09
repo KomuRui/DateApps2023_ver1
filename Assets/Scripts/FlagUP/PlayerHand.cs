e55ad939b07577d643814fa8941f05dd446107dd
咲奈 奈良岡 <210244@jc-21.jp>
2023-11-09 11:36:07 +0900

    はたあげ

--- Assets/Scripts/FlagUP/PlayerHand.cs
+++ Assets/Scripts/FlagUP/PlayerHand.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerHand : MonoBehaviour
 {
@@ -8,22 +9,54 @@ public class PlayerHand : MonoBehaviour
     public GameObject redOb;
     public GameObject whiteOb;
 
+    bool isLeftUp;
+    bool isRightUp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isLeftUp = false;
+        isRightUp = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("RBbutton" + playerNum))
+        if (Input.GetButtonDown("LBbutton" + playerNum))
         {
-            redOb.transform.eulerAngles = (new Vector3(0, 0, 90));
+            isLeftUp = true;
+            redOb.transform.DORotate(Vector3.forward * 0f, 0.1f);
         }
-        else if (Input.GetButtonDown("LBbutton" + playerNum))
+        else if (Input.GetButtonDown("RBbutton" + playerNum))
         {
-            whiteOb.transform.eulerAngles = (new Vector3(0, 0, 90));
+            isRightUp = true;
+            whiteOb.transform.DORotate(Vector3.forward * 90f, 0.1f);
         }
+        
+        //if(isLeftUp == true)
+        //{
+        //    if (redOb.transform.localEulerAngles.z >= 0)
+        //    {
+        //        redOb.transform.Rotate(0, 0, -10);
+        //    }
+        //    else
+        //    {
+        //        isLeftUp = false; 
+        //    }
+        //    //redOb.transform.eulerAngles = (new Vector3(0, 0, 0));
+        //}
+
+        //if (isLeftUp == true)
+        //{
+        //    if (redOb.transform.localEulerAngles.z >= 0)
+        //    {
+        //        redOb.transform.Rotate(0, 0, -10);
+        //    }
+        //    else
+        //    {
+        //        isLeftUp = false;
+        //    }
+        //    //redOb.transform.eulerAngles = (new Vector3(0, 0, 0));
+        //}
     }
 }
