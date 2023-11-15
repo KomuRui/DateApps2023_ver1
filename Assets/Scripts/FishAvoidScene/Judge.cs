facb2378717c0a1e80035a8f13fecb0e2d41591e
210244 <210244@jc-21.jp>
2023-11-15 15:11:01 +0900

    上げ下げ交互

--- Assets/Scripts/FishAvoidScene/Judge.cs
+++ Assets/Scripts/FishAvoidScene/Judge.cs
@@ -1,61 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Judge : MonoBehaviour
-{
-    public GameObject Player1;
-    public GameObject Player2;
-    public GameObject Player3;
-    public GameObject Player4;
+//public class Judge : MonoBehaviour
+//{
+//    public List<GameObject> Player;
 
-    public GameObject endText;
-    public GameObject[] ranking;
-    public List<int> temRanking;
+//    public GameObject endText;
+//    public GameObject[] ranking;
+//    public List<int> temRanking;
 
-    bool isJudge;
-    FishCountDown fishCountDown;
-    GameObject obj;
+//    bool isJudge;
+//    FishCountDown fishCountDown;
+//    GameObject obj;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        obj = GameObject.Find("GameManager");
-        fishCountDown = obj.GetComponent<FishCountDown>();
-        isJudge = false;
-    }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        obj = GameObject.Find("GameManager");
+//        fishCountDown = obj.GetComponent<FishCountDown>();
+//        isJudge = false;
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //ÇæÇÍÇ‡Ç¢Ç»Ç¢
-        if((Player1 == null && Player2 == null && Player3 == null) || fishCountDown.isFinish)
-        {
-            Instantiate(endText, new Vector3(0, 0, 0), Quaternion.identity);
-        }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        ////ÇæÇÍÇ‡Ç¢Ç»Ç¢
+//        if (((Player1 == null && Player2 == null && Player3 == null) || fishCountDown.isFinish) && isJudge == false)
+//        {
+//            Instantiate(endText, new Vector3(0, 0, 0), Quaternion.identity);
+//        }
 
-        if(fishCountDown.isFinish)
-        {
-            isJudge = true;
-            fishCountDown.isFinish = false;
-        }
+//        if (fishCountDown.isFinish)
+//        {
+//            isJudge = true;
+//            fishCountDown.isFinish = false;
+//        }
 
 
-        if(isJudge)
-        {
-            if(Player1 == null && Player2 == null && Player3 == null)
-            {
-                ranking[0] = Player4;
-                //éùÇ¡ÇƒÇ´Çƒ
-                //temRanking[0];
-            }
-            else
-            {
-                ranking[3] = Player4;
-                //éùÇ¡ÇƒÇ´Çƒ
+//        //if(isJudge)
+//        //{
+//        //    if(Player1 == null && Player2 == null && Player3 == null)
+//        //    {
+//        //        ranking[0] = Player4;
+//        //        //éùÇ¡ÇƒÇ´Çƒ
 
-            }
+//        //        //ranking[1] = temRanking[0];
 
-        }
-    }
-}
+//        //    }
+//        //    else
+//        //    {
+//        //        ranking[3] = Player4;
+//        //        //éùÇ¡ÇƒÇ´Çƒ
+
+//        //    }
+
+//        //}
+//    }
+//}
