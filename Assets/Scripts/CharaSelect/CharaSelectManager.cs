84a677c7e7cde6f803986088c05052df3dbc5f7a
KomuRui <103985106+KomuRui@users.noreply.github.com>
2023-11-19 13:50:33 +0900

    キャラ選択画面作成

--- Assets/Scripts/CharaSelect/CharaSelectManager.cs
+++ Assets/Scripts/CharaSelect/CharaSelectManager.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class CharaSelectManager : MonoBehaviour
 {
+    //ƒ‰ƒCƒ“”
+    enum LineNum
+    {
+        ONE,
+        TWO,
+        THREE,
+        MAX_LINE
+    }
+
+    [SerializeField] private List<GameObject> line1Chara;
+    [SerializeField] private List<GameObject> line2Chara;
+    [SerializeField] private List<GameObject> line3Chara;
+
+    private Dictionary<byte, List<GameObject>> lineCharaTable = new Dictionary<byte, List<GameObject>>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lineCharaTable[(byte)LineNum.ONE] = line1Chara;
+        lineCharaTable[(byte)LineNum.TWO] = line2Chara;
+        lineCharaTable[(byte)LineNum.THREE] = line3Chara;
     }
 
     // Update is called once per frame
