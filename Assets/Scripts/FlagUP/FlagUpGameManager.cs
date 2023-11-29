414d02f57e6b78d83babd0bfab5951c00873a4c2
琉偉 小村 <210465@jc-21.jp>
2023-11-29 16:40:31 +0900

    s

--- Assets/Scripts/FlagUP/FlagUpGameManager.cs
+++ Assets/Scripts/FlagUP/FlagUpGameManager.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static FlagUpGameManager;
 
 public class FlagUpGameManager : MiniGameManager
 {
-    public bool isAloneTurn;
-    public bool isStop;
-    public int roundNum;
 
-    // Start is called before the first frame update
+    //ターン
+    public enum Turn
+    {
+        ONE_PLAYER,
+        THREE_PLAYER
+    }
+
+    [SerializeField] private Transform mainCamera;             //メインカメラのトランスフォーム
+    [SerializeField] private Vector3 onePlayerTurnCameraPos;   //1人側ターンの時のカメラ位置
+    [SerializeField] private Vector3 threePlayerTurnCameraPos; //3人側ターンの時のカメラ位置
+
+    public Turn turn;       //どっちのターンか
+    public int nowRoundNum; //現在のラウンド数
+
     public override void SceneStart()
     {
-        isAloneTurn = true;
-        isStop = false;
-        roundNum = 0;
+        turn = Turn.ONE_PLAYER;
+        nowRoundNum = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //ターン変更
+    public void ChangeTurn() { turn = (turn == Turn.ONE_PLAYER) ? Turn.THREE_PLAYER : Turn.ONE_PLAYER; }
 }
--- Assets/Scripts/FlagUP/PlayerHand.cs
+++ Assets/Scripts/FlagUP/PlayerHand.cs
@@ -5,101 +5,86 @@ using DG.Tweening;
 
 public class PlayerHand : MonoBehaviour
 {
-    [SerializeField] private int playerNum;                   // プレイヤー番号
-    public GameObject leftOb;
-    public GameObject rightOb;
-    public GameObject GMOb;
-
-    public bool isInput;//1回
-    public bool isFirst;//1回
-
-    private int flagUpNum;
-    private int flagMax;
-
-    FlagUpGameManager flagUpGameManager;
+    [SerializeField] private int playerNum;      // プレイヤー番号
+    [SerializeField] public GameObject leftOb;   // 左の旗
+    [SerializeField] public GameObject rightOb;  // 右の旗
 
     // Start is called before the first frame update
     void Start()
     {
-        isInput = true;
-        isFirst = true;
-        flagUpNum = 0;
-        flagMax = 3;
-        //開始まで
-        Invoke("PlayPlayer", 5.0f);
-        flagUpGameManager = GMOb.GetComponent<FlagUpGameManager>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ストップしてない&自分のターン
-        if (flagUpGameManager.isStop == false && flagUpGameManager.isAloneTurn == false)
-        {
-            if (isInput)
-            {
-                if (Input.GetButtonDown("LBbutton" + playerNum))
-                {
-                    leftOb.transform.DORotate(Vector3.forward * -90f, 0.1f);
-                }
-                else if (Input.GetButtonDown("RBbutton" + playerNum))
-                {
-                    rightOb.transform.DORotate(Vector3.forward * 90f, 0.1f);
-                }
-                else if (Input.GetButtonDown("Abutton" + playerNum))
-                {
-                    leftOb.transform.DORotate(Vector3.forward * 0, 0.1f);
-                    rightOb.transform.DORotate(Vector3.forward * 0, 0.1f);
-                }
+        ////ストップしてない&自分のターン
+        //if (flagUpGameManager.isStop == false && flagUpGameManager.isAloneTurn == false)
+        //{
+        //    if (isInput)
+        //    {
+        //        if (Input.GetButtonDown("LBbutton" + playerNum))
+        //        {
+        //            leftOb.transform.DORotate(Vector3.forward * -90f, 0.1f);
+        //        }
+        //        else if (Input.GetButtonDown("RBbutton" + playerNum))
+        //        {
+        //            rightOb.transform.DORotate(Vector3.forward * 90f, 0.1f);
+        //        }
+        //        else if (Input.GetButtonDown("Abutton" + playerNum))
+        //        {
+        //            leftOb.transform.DORotate(Vector3.forward * 0, 0.1f);
+        //            rightOb.transform.DORotate(Vector3.forward * 0, 0.1f);
+        //        }
 
-                if (isFirst)
-                {
-                    isFirst = false;
-                    //上げれる時間
-                    Invoke("StopPlayer", 5.0f);
-                }
-            }
-            else
-            {
-                if (flagMax > flagUpNum && isFirst)
-                {
-                    isFirst = false;
+        //        if (isFirst)
+        //        {
+        //            isFirst = false;
+        //            //上げれる時間
+        //            Invoke("StopPlayer", 5.0f);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (flagMax > flagUpNum && isFirst)
+        //        {
+        //            isFirst = false;
 
-                    //1人側ターンにする
-                    flagUpGameManager.isAloneTurn = true;
+        //            //1人側ターンにする
+        //            flagUpGameManager.isAloneTurn = true;
 
-                    if (flagMax == flagUpNum)
-                    {
+        //            if (flagMax == flagUpNum)
+        //            {
 
-                        //旗上げ回数0に戻す
-                        flagUpNum = 0;
-                        //ラウンド3以上だったら旗上げ回数5
-                        if (flagUpGameManager.roundNum >= 2)
-                        {
-                            flagMax = 5;
-                        }
+        //                //旗上げ回数0に戻す
+        //                flagUpNum = 0;
+        //                //ラウンド3以上だったら旗上げ回数5
+        //                if (flagUpGameManager.roundNum >= 2)
+        //                {
+        //                    flagMax = 5;
+        //                }
 
-                        flagUpGameManager.roundNum++;
-                    }
+        //                flagUpGameManager.roundNum++;
+        //            }
 
-                    //あげれない時間
-                    Invoke("PlayPlayer", 5.0f);
-                }
+        //            //あげれない時間
+        //            Invoke("PlayPlayer", 5.0f);
+        //        }
 
-                if(flagUpGameManager.roundNum >= 3)
-                {
-                    flagUpGameManager.isStop = true;
-                }
-            }
-        }
+        //        if(flagUpGameManager.roundNum >= 3)
+        //        {
+        //            flagUpGameManager.isStop = true;
+        //        }
+        //    }
+        //}
     }
 
     //上げれない
     void StopPlayer()
     {
-        isInput = false;
-        isFirst = true;
-        flagUpNum++;
+        //isInput = false;
+        //isFirst = true;
+        //flagUpNum++;
         
         ////1人側ターンにする
         //flagUpGameManager.isAloneTurn = true;
@@ -123,7 +108,7 @@ public class PlayerHand : MonoBehaviour
         //笛鳴らす
 
 
-        isInput = true;
-        isFirst = true;
+        //isInput = true;
+        //isFirst = true;
     }
 }
