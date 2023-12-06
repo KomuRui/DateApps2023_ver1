641173be681f76cf2b214550c08e87f5b98813e7
三浦暁 <210525@jc-21.jp>
2023-12-06 13:09:06 +0900

    マージ解消

--- Assets/Scripts/ReachExtender/MagicHand.cs
+++ Assets/Scripts/ReachExtender/MagicHand.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class MagicHand : MonoBehaviour
 {
-    [SerializeField] private GameObject onePlayer;
+    //[SerializeField] private GameObject onePlayer;
     // Start is called before the first frame update
     void Start()
     {
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z + 0.001f);
     }
 }
