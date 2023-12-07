1a1352a4ed6243cecc16ceac2cc933d4267af40e
三浦暁 <210525@jc-21.jp>
2023-12-07 18:28:05 +0900

    a

--- Assets/Scripts/ReachExtender/MagicHand.cs
+++ Assets/Scripts/ReachExtender/MagicHand.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static DG.Tweening.DOTweenModuleUtils;
 
 public class MagicHand : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class MagicHand : MonoBehaviour
     private bool bigMax = false;        //マジックハンドが伸びきったかどうか
     [SerializeField] private GameObject nextArmParent;
     [SerializeField] private GameObject nextArmParentTop;
+    [SerializeField] private GameObject myArmParentTop;
 
 
     void Start()
@@ -38,6 +40,8 @@ public class MagicHand : MonoBehaviour
         bigMax = true;
         if(nextArmParent != null)
         {
+            Vector3 ray = transform.parent.gameObject.transform.position - myArmParentTop.gameObject.transform.position;
+            //Physics.Raycast(Vector3 origin(rayの開始地点), Vector3 direction(rayの向き), RaycastHit hitInfo(当たったオブジェクトの情報を格納), float distance(rayの発射距離), int layerMask(レイヤマスクの設定));
             nextArmParent.SetActive(true);
         }
     }
