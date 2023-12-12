d603fd0b84899e98106681e3cccc4e4ba9e960bf
琉偉 小村 <210465@jc-21.jp>
2023-12-12 16:28:28 +0900

    a

---
+++ Assets/Scripts/DriveChaseFish/NetCollider.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetCollider : MonoBehaviour
+{
+
+    public List<GameObject> fishObj = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Fishes") 
+            fishObj.Add(collision.gameObject);
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.transform.tag == "Fishes") 
+            fishObj.Remove(collision.gameObject);
+    }
+}
