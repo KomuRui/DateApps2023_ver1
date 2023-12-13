8137032e47599a3e5ad2ce8afea94530200f4f3a
三浦暁 <210525@jc-21.jp>
2023-12-13 10:22:29 +0900

    a

---
+++ Assets/Scripts/DeathRun/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Vector3 MoveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMoveDirection(Vector3 moveDirection)
+    {
+
+    }
+}
---
+++ Assets/Scripts/DeathRun/Cannon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : GimmickBase
+{
+    [SerializeField] private GameObject shotPos;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float bulletSpeed = 15000000f;
+
+    public override void Action()
+    {
+        //球を発射
+        Shot();
+    }
+
+    public override void GimmickStart()
+    {
+    }
+
+    public override void GimmickUpdate()
+    {
+    }
+
+    //球を発射
+    public void Shot()
+    {
+        GameObject createdBullet = Instantiate(bullet) as GameObject;
+        createdBullet.transform.position = shotPos.transform.position;
+
+        //発射ベクトル
+        Vector3 force = new Vector3(1,0,0);
+
+        //発射の向きと速度を決定
+        force *= bulletSpeed * Time.deltaTime;
+
+        // Rigidbodyに力を加えて発射
+        createdBullet.GetComponent<Rigidbody>().AddForce(force);
+    }
+}
