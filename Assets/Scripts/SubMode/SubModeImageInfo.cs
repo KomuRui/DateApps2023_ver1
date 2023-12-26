86a24b68e7c157913bf42d9fb7009bbd7c2b1861
琉偉 小村 <rui.rui.55@outlook.jp>
2023-12-26 14:39:04 +0900

    sabu

--- Assets/Scripts/SubMode/SubModeImageInfo.cs
+++ Assets/Scripts/SubMode/SubModeImageInfo.cs
@@ -1,26 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SubModeImageInfo : MonoBehaviour
 {
+    //宝庫
+    public enum Direction
+    {
+        RIGHT,
+        LEFT,
+        UP,
+        DOWN,
+        MAX_DIRECTION
+    }
 
     [SerializeField] private SubModeImageInfo leftImage;    //左側の画像
     [SerializeField] private SubModeImageInfo rightImage;   //右側の画像
     [SerializeField] private SubModeImageInfo upImage;      //上側の画像
     [SerializeField] private SubModeImageInfo downImage;    //下側の画像
     [SerializeField] private List<Image> edgeImage;         //枠の画像
-    [SerializeField] private List<Image> playerNumberImage; //枠の画像
+    [SerializeField] private List<Image> playerNumberImage; //プレイヤー番号の画像
+
+    //どの方向に選択画像があるか
+    private Dictionary<Direction, SubModeImageInfo> dirSelectImage = new Dictionary<Direction, SubModeImageInfo>();
+
+    //何人のプレイヤーから選択されているか
+    public List<byte> playerSelectMyNum;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        //枠の画像たちを透明に設定
+        for(int i = 0; i < edgeImage.Count; i++)
+        {
+            Color a = edgeImage[i].color;
+            Color b = playerNumberImage[i].color;
+            a.r = 0;
+            b.r = 0;
+            edgeImage[i].color = a;
+            playerNumberImage[i].color = b;
+        }
+
+        //各方向の画像を設定
+        dirSelectImage[Direction.RIGHT] = rightImage;
+        dirSelectImage[Direction.LEFT] = leftImage;
+        dirSelectImage[Direction.UP] = upImage;
+        dirSelectImage[Direction.DOWN] = downImage;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //選択画像を変更(変更できたのなら変更先を返す)
+    public SubModeImageInfo SelectImageChange(byte playerNum, Dictionary<byte, SubModeSelectManager.InputInfo> input)
+    {
+        if (IsInputOK(playerNum, Direction.RIGHT, input) && dirSelectImage[Direction.RIGHT]) return dirSelectImage[Direction.RIGHT];
+        else if (IsInputOK(playerNum, Direction.LEFT, input) && dirSelectImage[Direction.LEFT]) return dirSelectImage[Direction.LEFT];
+        else if (IsInputOK(playerNum, Direction.UP, input) && dirSelectImage[Direction.UP]) return dirSelectImage[Direction.UP];
+        else if (IsInputOK(playerNum, Direction.DOWN, input) && dirSelectImage[Direction.DOWN]) return dirSelectImage[Direction.DOWN];
+
+        return null;
+    }
+
+    //入力がOKかどうか
+    private bool IsInputOK(byte playerNum,Direction dir,Dictionary<byte, SubModeSelectManager.InputInfo> input)
+    {
+        switch (dir)
+        {
+            case Direction.RIGHT:
+                if (input[playerNum].beforeInputX <= 0.799 && input[playerNum].nowInputX >= 0.8)
+                    return true;
+                else
+                    return false;
+            case Direction.LEFT:
+                if (input[playerNum].beforeInputX >= -0.799 && input[playerNum].nowInputX <= -0.8)
+                    return true;
+                else
+                    return false;
+            case Direction.DOWN:
+                if (input[playerNum].beforeInputY <= 0.799 && input[playerNum].nowInputY >= 0.8)
+                    return true;
+                else
+                    return false;
+            case Direction.UP:
+                if (input[playerNum].beforeInputY >= -0.799 && input[playerNum].nowInputY <= -0.8)
+                    return true;
+                else
+                    return false;
+        }
+
+        return false;
+    }
+
+    //画像の色変更
+    public void ImageColorChange()
+    {
+
     }
 }
--- Assets/Scripts/SubMode/SubModeSelectManager.cs
+++ Assets/Scripts/SubMode/SubModeSelectManager.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class SubModeSelectManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //入力情報
+    public class InputInfo
+    {
+        public float nowInputX;
+        public float nowInputY;
+        public float beforeInputX;
+        public float beforeInputY;
+    }
+
+    //プレイヤーの初期選択画像
+    [SerializeField] private List<SubModeImageInfo> playerInitializSelectImafe;
+
+    //プレイヤーの色
+    [SerializeField] public List<Color> playerColor;
+
+    //情報保管しているところ
+    private Dictionary<byte, SubModeImageInfo> playerSelectImage = new Dictionary<byte, SubModeImageInfo>();
+    private Dictionary<byte, InputInfo> inputXY = new Dictionary<byte, InputInfo>();
+
     void Start()
     {
-        
+        //初期化
+        for (byte i = 0; i < PlayerManager.PLAYER_MAX; i++)
+        {
+            //入力値を初期化
+            InputInfo input = new InputInfo();
+            input.nowInputX = 0;
+            input.nowInputY = 0;
+            input.beforeInputX = 0;
+            input.beforeInputY = 0;
+            inputXY[(byte)(i + 1)] = input;
+
+            //プレイヤーの初期選択画像を初期化
+            playerSelectImage[(byte)(i + 1)] = playerInitializSelectImafe[i];
+            playerSelectImage[(byte)(i + 1)].playerSelectMyNum.Add((byte)(i + 1));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (byte i = 1; i < PlayerManager.PLAYER_MAX + 1; i++)
+        {
+            //今回の入力値を取得
+            inputXY[i].nowInputX = Input.GetAxis("L_Stick_H" + i);
+            inputXY[i].nowInputY = Input.GetAxis("L_Stick_V" + i);
+
+            //変更ができたかチェック
+            SubModeImageInfo info = playerSelectImage[i].SelectImageChange(i, inputXY);
+            if (info) playerSelectImage[i] = info;
+
+            //今回の入力値を保存
+            inputXY[i].beforeInputX = inputXY[i].nowInputX;
+            inputXY[i].beforeInputY = inputXY[i].nowInputY;
+        }
     }
 }
