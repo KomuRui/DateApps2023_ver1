a215d3fddc413e668dc1c794f6d523fb2a9663dc
琉偉 小村 <rui.rui.55@outlook.jp>
2023-12-29 23:12:32 +0900

    a

--- Assets/Scripts/FlagUP/ProPera.cs
+++ Assets/Scripts/FlagUP/ProPera.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class ProPera : MonoBehaviour
 {
-
+    [SerializeField] private float speed;
+    private float angle;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        angle = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        angle += speed + Time.deltaTime;
+        transform.rotation = Quaternion.AngleAxis(angle, this.transform.up);
     }
 }
