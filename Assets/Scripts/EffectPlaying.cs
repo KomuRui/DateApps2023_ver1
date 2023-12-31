f14bc0ba602f02372937cd8dfc0ef7f12dad2939
琉偉 小村 <rui.rui.55@outlook.jp>
2023-12-31 19:59:23 +0900

    バグ修正

---
+++ Assets/Scripts/EffectPlaying.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectOkaying : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<ParticleSystem>().isPaused)
+            this.GetComponent<ParticleSystem>().Play();
+    }
+}
