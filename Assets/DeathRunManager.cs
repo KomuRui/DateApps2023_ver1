5a97669add1a27122b62b0fac701a8233f5cd4c8
三浦暁 <210525@jc-21.jp>
2024-01-09 11:35:15 +0900

    aa

---
+++ Assets/DeathRunManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathRunManager : MiniGameManager
+{
+    //Startと同じ
+    public override void SceneStart()
+    {
+    }
+
+    //ゲーム開始時に呼ばれる
+    public override void MiniGameStart()
+    {
+    }
+
+    //Updateと同じ
+    public override void MiniGameUpdate()
+    {
+    }
+
+    //ミニゲームが終わったら呼ばれる
+    public override void MiniGameFinish()
+    {
+    }
+}
