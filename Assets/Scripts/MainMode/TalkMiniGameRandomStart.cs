f07d5741c235c6530162da706566cd59ac0462d1
KomuRui <rui.rui.55@icloud.com>
2024-01-15 15:24:17 +0900

    ルーレットのところまで

--- Assets/Scripts/MainMode/TalkMiniGameRandomStart.cs
+++ Assets/Scripts/MainMode/TalkMiniGameRandomStart.cs
@@ -9,12 +9,18 @@ public class TalkMiniGameRandomStart : talkText
     [SerializeField] private GameObject mainMiniGameBoard;
     [SerializeField] private GameObject talkSignBorad;
     [SerializeField] private GameObject mc;
+    [SerializeField] private StageSelect stageSelect;
 
     //すべての会話終了したときの処理
     public override void AllTalkFinish() 
     {
+        //アニメーション
         mainMiniGameBoard.transform.DOMoveY(-0.2f, 2.0f).SetEase(Ease.OutQuart);
         talkSignBorad.transform.DOMoveY(25, 2.0f).SetEase(Ease.OutQuart);
         mc.transform.DOMoveZ(30, 2.0f).SetEase(Ease.OutQuart);
+
+        //ミニゲームランダムスタート
+        StartCoroutine(stageSelect.MiniGameRandom(3.0f));
     }
+
 }
