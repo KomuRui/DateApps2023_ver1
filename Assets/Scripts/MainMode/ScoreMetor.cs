19b5a64d35e311252531fa69f38715f0d2947b04
琉偉 小村 <210465@jc-21.jp>
2024-01-16 18:24:39 +0900

    a

---
+++ Assets/Scripts/MainMode/ScoreMetor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMetor : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> metor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
