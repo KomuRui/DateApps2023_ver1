7a9634fc088e68c559e40e87b7f7f6df95b2dd4b
三浦暁 <210525@jc-21.jp>
2024-01-17 11:36:21 +0900

    追加

--- Assets/Scripts/ReachExtender/MagicHandIsHit.cs
+++ Assets/Scripts/ReachExtender/MagicHandIsHit.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MagicHandIsHit : MonoBehaviour
 {
-
     public bool isHit = false;
     // Start is called before the first frame update
     void Start()
@@ -19,8 +18,6 @@ public class MagicHandIsHit : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        //if (other.tag == "Arm") return;
-
         if (other.tag == "Stage")
         {
             isHit = true;
